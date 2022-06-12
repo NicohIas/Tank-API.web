822ddbf23248a7c9c5efa57819ff4ad8ed16455e
nic <93119331+NicohIas@users.noreply.github.com>
2022-06-12 17:04:55 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log (
+    "%cTank API\n%cHey! If someone told you to paste something here, there's a 73.56789198651273923781287% chance you're being scammed.\n%cAlso, if you're into diving deeper into Tank API, there is no reason to look here.",
+    "color:#013220;font-size:48px;font-weight:600;",
+    "color:green;font-size:20px;",
+    "color:#90EE90;font-size:12px;"
+);
